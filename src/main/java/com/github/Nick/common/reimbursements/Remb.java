53319ebc443b3b26aef713f2af34367d8a7d8576
NickNSX <nickogir@gmail.com>
2022-09-12 20:09:46 -0500

    started reimburesment class

--- src/main/java/com/github/Nick/common/reimbursements/Remb.java
+++ src/main/java/com/github/Nick/common/reimbursements/Remb.java
@@ -2,4 +2,16 @@ package com.github.Nick.common.reimbursements;
 
 public class Remb {
     
+    private String reimb_id;
+    private int amount;
+    private String submitted;
+    private String resolved;
+    private String description;
+    private String payment;
+    
+    
+    
+    
+    
+    
 }
