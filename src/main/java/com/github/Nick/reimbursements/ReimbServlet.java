9169290aa6ea9a03f54579310afc48efc4852d28
NickNSX <nickogir@gmail.com>
2022-09-13 20:11:19 -0500

    Created check for only authorized roles

--- src/main/java/com/github/Nick/reimbursements/ReimbServlet.java
+++ src/main/java/com/github/Nick/reimbursements/ReimbServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpSession;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.Nick.common.ErrorResponse;
+import com.github.Nick.users.UserResponse;
 
 public class ReimbServlet extends HttpServlet {
 
@@ -30,6 +31,17 @@ public class ReimbServlet extends HttpServlet {
         if (reimbSession == null) {
             resp.setStatus(401);
             resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(401, "Requestor not authenticated with server, log in")));
+            return;
+        }
+
+        UserResponse requester = (UserResponse) reimbSession.getAttribute("authUser");
+
+        String idToSearchFor = req.getParameter("id");
+        String statusToSearchFor = req.getParameter("status");
+
+        if ((!requester.getRole().equals("CEO") && !requester.getRole().equals("FINANCE MANAGER")) && !requester.getUser_id().equals(idToSearchFor) && !requester.getRole().equals(idToSearchFor)) {
+            resp.setStatus(403); // Forbidden
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(403, "Requester not permitted to communicate with this endpoint.")));
             return;
         }
 
